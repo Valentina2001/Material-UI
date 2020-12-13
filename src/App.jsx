21b4b39f5d2da535b18cef5138c9eb3549f8b380
Valentina Caldera Hinestroza <valen.caldera17@gmail.com>
2020-12-12 22:32:18 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import {Button} from "@material-ui/core"
+
+function App() {
+  return (
+    <div>
+      <Button color="secondary">
+        Color
+      </Button>
+      <Button 
+      color="secondary"
+      variant="contained"
+      >
+        Outlined
+      </Button>
+      <p>Lorem</p>
+      <Button 
+      color="primary"
+      variant="contained"
+      /*disableElevation*/
+      href="https://google.com"
+      >
+        Google
+      </Button> 
+      <Button 
+      color="primary"
+      variant="contained"
+      /*disableElevation*/
+      fullWidth
+      >
+        Fullwidth
+      </Button> 
+
+      <Button 
+      color="primary"
+      variant="contained"
+      size="small"
+      >
+        Fullwidth
+      </Button> 
+
+      <Button 
+      color="primary"
+      variant="contained"
+      size="medium"
+      >
+        Fullwidth
+      </Button> 
+
+      <Button 
+      color="primary"
+      variant="contained"
+      size="large"
+      >
+        Fullwidth
+      </Button> 
+    </div>
+  );
+}
+
+export default App;
